cc1320825beaae4d3f4d378465d93f5c65ae8ea5
zhourusheng <zhourusheng@yunzhangfang.com>
2020-12-07 16:27:02 +0800

    调整目录

--- src/components/FixedSizeList/index.tsx
+++ src/components/FixedSizeList/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { FixedSizeList as List } from 'react-window'
+import './index.css'
 
 // @ts-ignore
 const Row = ({ index, style }) => (
-  <div className={index % 2 ? 'listItemOdd' : 'listItemEven'}>Row {index}</div>
+  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
+    Row {index}
+  </div>
 )
 
 const FixedSizeList = () => {
   return (
     <List
-      className='list'
+      className='List'
       height={150}
       itemCount={1000}
       itemSize={35}
