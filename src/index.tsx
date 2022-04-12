c9baccf84a97d3aad8f9dabbaf003923f01c074e
zhourusheng <nfuzrs@126.com>
2022-04-12 17:56:49 +0800

    创建一个 link 标签, 动态写入 href

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,17 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
+// 创建一个 link 标签, 动态写入 href
+const HEAD = window.document.getElementsByTagName('HEAD')
+const Style = document.createElement('link')
+const href = `https://fintax-web-1257122416.cos.ap-shanghai.myqcloud.com/yzf-antd4/${
+  process.env.NODE_ENV === 'production' ? 'production' : 'develop'
+}/index.css`
+Style.href = href
+Style.rel = 'stylesheet'
+Style.type = 'text/css'
+HEAD && HEAD[0] && HEAD[0].appendChild(Style)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
